3fb83d1e39c28524854a955615a49963e1809089
Julian Camilo Benavides Vanegas <correoj94@gmail.com>
2025-02-21 22:22:38 -0500

    :sparkless: layers created

--- SistemaEstudiantes.Application/Services/EstudianteService.cs
+++ SistemaEstudiantes.Application/Services/EstudianteService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SistemaEstudiantes.Domain.Entities;
+using SistemaEstudiantes.Domain.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,37 @@ using System.Threading.Tasks;
 
 namespace SistemaEstudiantes.Application.Services
 {
-    internal class EstudianteService
+    public class EstudianteService
     {
+        private readonly IEstudianteRepository _estudianteRepository;
+        public EstudianteService(IEstudianteRepository estudianteRepository)
+        {
+            _estudianteRepository = estudianteRepository;
+        }
+
+        public async Task<IEnumerable<Estudiante>> GetAllAsync()
+        {
+            return await _estudianteRepository.GetAllAsync();
+        }
+
+        public async Task<Estudiante> GetByIdAsync(int id)
+        {
+            return await _estudianteRepository.GetByIdAsync(id);
+        }
+
+        public async Task AddAsync(Estudiante estudiante)
+        {
+            await _estudianteRepository.AddAsync(estudiante);
+        }
+
+        public async Task UpdateAsync(Estudiante estudiante)
+        {
+            await _estudianteRepository.UpdateAsync(estudiante);
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            await _estudianteRepository.DeleteAsync(id);
+        }
     }
 }
---
+++ SistemaEstudiantes.Domain/Interfaces/IEstudianteRepository.cs
@@ -0,0 +1,18 @@
+﻿using SistemaEstudiantes.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaEstudiantes.Domain.Interfaces
+{
+    public interface IEstudianteRepository
+    {
+        Task<IEnumerable<Estudiante>> GetAllAsync();
+        Task<Estudiante> GetByIdAsync(int id);
+        Task AddAsync(Estudiante estudiante);
+        Task UpdateAsync(Estudiante estudiante);
+        Task DeleteAsync(int id);
+    }
+}
