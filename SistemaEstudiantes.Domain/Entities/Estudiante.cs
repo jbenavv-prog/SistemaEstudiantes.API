fa35277cd03f45c6de3ec45943a2e5fb31db50e2
Julian Camilo Benavides Vanegas <correoj94@gmail.com>
2025-02-21 21:52:43 -0500

    Add project files.

---
+++ SistemaEstudiantes.Domain/Entities/Estudiante.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaEstudiantes.Domain.Entities
+{
+    public class Estudiante
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public int IDPrograma { get; set; }
+    }
+}
---
+++ SistemaEstudiantes.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaEstudiantes.Domain.Entities;
+
+namespace SistemaEstudiantes.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Estudiante> Estudiantes { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+/*            modelBuilder.Entity<Estudiante>().ToTable("Estudiantes");
+*/        }
+    }
+}
