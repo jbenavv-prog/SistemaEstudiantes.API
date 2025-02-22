9a3c2f7237678d6613942b8d9836bb62800effc2
Julian Camilo Benavides Vanegas <correoj94@gmail.com>
2025-02-22 08:33:48 -0500

    :sparkless: migration and first verb works with database

--- SistemaEstudiantes.Infrastructure/Data/AppDbContext.cs
+++ SistemaEstudiantes.Infrastructure/Data/AppDbContext.cs
@@ -6,10 +6,16 @@ namespace SistemaEstudiantes.Infrastructure.Data
     public class AppDbContext : DbContext
     {
         public DbSet<Estudiante> Estudiantes { get; set; }
+        public AppDbContext() { }
+        // Constructor con parámetros (usado en tiempo de ejecución)
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-/*            modelBuilder.Entity<Estudiante>().ToTable("Estudiantes");
-*/        }
+            modelBuilder.Entity<Estudiante>(entity => {
+                entity.HasKey(e => e.ID);
+                entity.Property(e => e.Nombre).IsRequired().HasMaxLength(100);
+            });
+        }
     }
 }
---
+++ SistemaEstudiantes.Infrastructure/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace SistemaEstudiantes.Infrastructure.Data
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var basePath = Path.Combine(Directory.GetCurrentDirectory(), "..", "SistemaEstudiantes.API");
+            // Configurar la configuración desde appsettings.json
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(basePath) // Establecer la ruta base
+                .AddJsonFile("appsettings.json") // Agregar el archivo de configuración JSON
+                .Build();
+
+            // Configurar el DbContextOptionsBuilder
+            var builder = new DbContextOptionsBuilder<AppDbContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            builder.UseSqlServer(connectionString);
+
+            // Crear una instancia de AppDbContext
+            return new AppDbContext(builder.Options);
+        }
+    }
+}
