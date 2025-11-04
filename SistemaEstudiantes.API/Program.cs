71e0548728b92591b2ec11ea7f21a5c33fbdb0cc
ingeniero.master38 <julianbenavidesv@interrapidisimo.online>
2025-11-04 17:55:19 -0500

    swagger works in production mode

--- SistemaEstudiantes.API/Program.cs
+++ SistemaEstudiantes.API/Program.cs
@@ -45,8 +45,7 @@ builder.Services.AddScoped<IJwtProvider, JwtProvider>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
